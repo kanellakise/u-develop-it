5877cef48bdf799cae0c6c587d59ba6c9d348c7f
DeathByDusk <elijahkanellakis@gmail.com>
2021-11-29 23:00:38 -0600

    connected database to express server

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mysql = require('mysql2');
 const express = require('express');
 const PORT = process.env.PORT || 3001;
 const app = express();
@@ -6,10 +7,25 @@ const app = express();
 app.use(express.urlencoded({ extended: false }));
 app.use(express.json());
 
+// Connect to database
+const db = mysql.createConnection(
+    {
+        host: 'localhost',
+        user: 'root',
+        password: '2xu^#8F#kbE2',
+        database: 'election'
+    },
+    console.log('Connected to the election database.')
+);
+
 app.get('/', (req, res) => {
     res.json({
         message: 'Hello World!'
     });
+});
+
+db.query(`SELECT * FROM candidates`, (err, rows) => {
+    console.log(rows);
 });
 
 // Default response for any other request (Not Found)
