206f31ae166afa25b1dee397b73a536d5f7f6760
DeathByDusk <elijahkanellakis@gmail.com>
2021-12-07 10:48:44 -0600

    organized routes

---
+++ routes/apiRoutes/partyRoutes.js
@@ -0,0 +1,20 @@
+const express = require('express');
+const router = express.Router();
+const db = require('../../db/connection');
+
+// Get all parties
+router.get('/parties', (req, res) => {
+
+});
+
+// Get single party
+router.get('/party/:id', (req, res) => {
+
+});
+
+// Delete a party
+router.delete('/party/:id', (req, res) => {
+
+});
+
+module.exports = router;
